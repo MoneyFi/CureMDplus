a45c496cd988908d5e821625995db06b62a14a0b
LeandroNGarcia <leandromaxi2014@gmail.com>
2024-05-10 17:33:24 -0300

    Update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'white': '#ffffff',
+      'primary-blue': '#283E94',
+      'primary-skyblue': '#4AB0C4',
+      'secondary-blue': '#1655A4',
+      'secondary-skyblue': '#239FD8',
+      'alternate': '#4D4D4D',
+    },
+    fontFamily: {
+      'sans': ['Open Sans', 'sans-serif'],
+      'roboto': ['Roboto', 'sans-serif'],
+      'varela': ['Varela Round', 'sans-serif'],
+    }
   },
   plugins: [],
 }
