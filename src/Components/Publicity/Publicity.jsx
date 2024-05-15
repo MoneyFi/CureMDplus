41dba315fda574e56615730aa5b9a206781be3ef
LeandroNGarcia <leandromaxi2014@gmail.com>
2024-05-15 16:59:10 -0300

    update styles

--- src/Components/Publicity/Publicity.jsx
+++ src/Components/Publicity/Publicity.jsx
@@ -4,14 +4,12 @@ import app from '../../assets/icons/cureMDmobile.png'
 
 const Publicity = () => {
   return (
-    <div id='publicity' className='w-full min-h-[100vh] flex justify-center items-center flex-wrap p-[3em]'>
-      <div className='w-[100%] md:w-1/2 lg:w-1/3 px-4'>
-        <img src={mobile} alt="" className='w-full md:max-w-xs lg:max-w-sm mx-auto mb-8 md:mb-0' id='mobile' />
-      </div>
-      <div className='w-full md:w-1/2 lg:w-2/3 px-4 flex flex-col justify-center items-center'>
-        <img src={app} alt="Hola" className='w-full md:max-w-md lg:max-w-lg mx-auto mb-8' />
-        <h2 className='text-secondary-blue text-[3em] font-sans font-light mb-4 md:mb-6 text-center'>App de <b className='font-bold'>Telemedicina</b></h2>
-        <p className='text-secondary-blue font-sans font-light text-[1.5em] md:text-[2em] w-full md:max-w-lg text-center'>Planes accesibles, atención personalizada y tecnología de vanguardia para cuidar tu salud.</p>
+    <div id='publicity' className='w-full min-h-[100vh] flex justify-center items-center flex-wrap p-12 gap-0 md:gap-4'>
+      <img src={mobile} alt="" className='w-[200px] md:w-[300px]' id='mobile' />
+      <div className='md:w-3/4 px-4 flex flex-col md:flex justify-center items-center gap-4'>
+        <img src={app} alt="Hola" className='w-full md:w-2/4 mb-4' />
+        <h2 className='text-secondary-blue text-2xl md:text-6xl font-sans font-light mb-4 md:mb-6 text-center'>App de <b className='font-bold'>Telemedicina</b></h2>
+        <p className='text-secondary-blue font-sans font-light text-lg md:text-2xl md:max-w text-center'>Planes accesibles, atención personalizada y tecnología de vanguardia para cuidar tu salud.</p>
       </div>
     </div>
   )
