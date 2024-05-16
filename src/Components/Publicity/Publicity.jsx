58cfbdc297260c9c80cf5f4958aa54a329392c47
LeandroNGarcia <leandromaxi2014@gmail.com>
2024-05-16 13:29:53 -0300

    add: faqs-section

--- src/Components/Publicity/Publicity.jsx
+++ src/Components/Publicity/Publicity.jsx
@@ -5,7 +5,7 @@ import app from '../../assets/icons/cureMDmobile.png'
 const Publicity = () => {
   return (
     <div id='publicity' className='w-full min-h-[100vh] flex justify-center items-center flex-wrap p-12 gap-0 md:gap-4'>
-      <img src={mobile} alt="" className='w-[200px] md:w-[300px]' id='mobile' />
+      <img src={mobile} alt="" className='hidden md:w-[250px] md:block' id='mobile' />
       <div className='md:w-3/4 px-4 flex flex-col md:flex justify-center items-center gap-4'>
         <img src={app} alt="Hola" className='w-full md:w-2/4 mb-4' />
         <h2 className='text-secondary-blue text-2xl md:text-6xl font-sans font-light mb-4 md:mb-6 text-center'>App de <b className='font-bold'>Telemedicina</b></h2>
