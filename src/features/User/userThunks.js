7f622db8e696f1514d7bfe4a367fd0301434edd2
Lean Garcia <142270724+LeandroNGarcia@users.noreply.github.com>
2024-06-11 21:52:30 -0300

    Revert "pdf descargable"

--- src/features/User/userThunks.js
+++ src/features/User/userThunks.js
@@ -1,8 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { apiCallProd, base_s3, apiCallTest } from "../../Constants/Constants";
+import { apiCallProd, base_s3 } from "../../Constants/Constants";
 
-let apiCall = apiCallTest;
+let apiCall = apiCallProd
 
 export const registerUserThunk = createAsyncThunk(
   "registerUser",
