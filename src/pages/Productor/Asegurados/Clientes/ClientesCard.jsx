cb3a98d9fcc24e163995c746368b5160c99a0f71
Nahuel Cempellin <nahuelcempellin@gmail.com>
2024-07-02 16:02:48 -0300

    add: cards de clientes responsive

--- src/pages/Productor/Asegurados/Clientes/ClientesCard.jsx
+++ src/pages/Productor/Asegurados/Clientes/ClientesCard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ClientesCard = ({nombre, dni, poliza, vencimiento, estado, plan}) => {
   return (
-    <li className='w-full p-4 bg-[#d9d9d9] rounded flex items-center justify-around text-sm mb-2'>
+    <li className='w-full p-4 bg-[#d9d9d9] rounded flex items-center justify-around text-sm mb-2 max-[750px]:flex-col max-[750px]:items-start'>
+
+        <div className='flex w-[30%] p-2 justify-between items-center max-[750px]:w-full'>
               <div className='flex flex-col items-start'>
               <p className='font-bold text-primary-blue'>{nombre} </p>
               <p className=''>{plan}</p>
@@ -12,18 +14,25 @@ const ClientesCard = ({nombre, dni, poliza, vencimiento, estado, plan}) => {
               <p className='font-bold text-primary-blue'>DNI:</p>
               <p>{dni}</p>
               </div>
-              
-              <div >
+        </div>
+
+
+              <div className='p-2' >
               <p className='font-bold text-primary-blue'>Poliza:</p>
               <p>{poliza}</p>
               </div>
+              
 
+     <div className='flex p-2 w-[30%] justify-between items-center max-[750px]:w-full'>
               <div>
               <p className='font-bold text-primary-blue'>Fecha de vencimiento:</p>
               <p>{vencimiento}</p>
               </div>
 
               <p className={`font-bold ${estado === 'Activo' ? 'text-[#009700]' : 'text-[#ff0f0f]'}`}>{estado}</p>
+
+     </div>         
+
 
             </li>
   )
