78fe78dcb56e5832423c9a55723edffe3c6c0c9d
LeandroNGarcia <leandromaxi2014@gmail.com>
2024-07-17 10:35:05 -0300

    add: panel admin

--- src/pages/Register/FormLogin.jsx
+++ src/pages/Register/FormLogin.jsx
@@ -35,6 +35,7 @@ const FormLogin = ({ setHaveAccount, position, setPosition }) => {
         if (loading) {
             if (response === 'success') {
                 dispatch(createToast('Inicio exitoso, corroborando datos...'))
+                localStorage.setItem('para_actualizar', JSON.stringify(data))
                 setTimeout(() => {
                     if (login.data_user.cuil === '0') {
                         setPosition(2)
